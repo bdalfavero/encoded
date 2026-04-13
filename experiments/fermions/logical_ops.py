8d4d449ebec60b6f78945abbeb0be2a337240828
Benjamin DalFavero <bdalfavero@gmail.com>
2026-04-13 14:59:33 -0400

    Got logical operators of both codes.

--- experiments/fermions/logical_ops.py
+++ experiments/fermions/logical_ops.py
@@ -1,6 +1,16 @@
 import stim
 from encoded.utils import get_observables
 
+print("Four-qubit code")
+generators = [
+    stim.PauliString("ZIZII"),
+    stim.PauliString("IZIZI"),
+    stim.PauliString("IZZIZ")
+]
+logical_ops = get_observables(generators)
+for lop in logical_ops:
+    print(lop)
+print("Eight-qubit code")
 generators = [
     stim.PauliString("ZIZIZIZIII"),
     stim.PauliString("IZIZIZIZII"),
