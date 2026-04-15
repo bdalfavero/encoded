865430f8517fad0e0bd313f862d12951e7180bde
Benjamin DalFavero <bdalfavero@gmail.com>
2026-04-15 14:37:46 -0400

    Printed logical eigenvectors. For at least these parameters, they are computational basis states

--- experiments/fermions/logical_ops.py
+++ experiments/fermions/logical_ops.py
@@ -58,6 +58,8 @@ print("Logical eigenvalues")
 logical_ham_matrix = logical_hamiltonian.matrix(logical_qs)
 logical_eigvals, logical_eigvecs = eigh(logical_ham_matrix)
 print(logical_eigvals)
+for i, energy in enumerate(logical_eigvals):
+    print(energy, logical_eigvecs[:, i])
 
 fig, ax = plt.subplots()
 ax.hlines(phys_eigvals, 1., 2., colors=["blue"], label="Physical")
