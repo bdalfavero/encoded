752372aff4a0e751a83b2a2983c8666cf47cf476
Benjamin DalFavero <bdalfavero@gmail.com>
2026-04-17 11:00:15 -0400

    Refactor: Moved binary linear algebra stuff to its own module.

--- encoded/decompose_operators.py
+++ encoded/decompose_operators.py
@@ -1,73 +1,8 @@
 from typing import List, Tuple
 from math import prod
 import numpy as np
-from scipy.optimize import milp, LinearConstraint
 import stim
-
-def _swap_row(arr: np.ndarray, i: int, j: int):
-    temp = arr[i, :].copy()
-    arr_copy = arr.copy()
-    arr_copy[i, :] = arr_copy[j, :]
-    arr_copy[j, :] = temp
-    return arr_copy
-
-
-def _swap_elems(b: np.ndarray, i, j):
-    b_copy = b.copy()
-    temp = b[i]
-    b_copy[i] = b[j]
-    b_copy[j] = temp
-    return b_copy
-
-
-def _boolean_rref(A: np.ndarray, b: np.ndarray) -> np.ndarray:
-    # assert A.shape[1] <= A.shape[0]
-
-    A_copy = A.copy()
-    b_copy = b.copy()
-
-    for j in range(A.shape[1]):
-        # Find the first index i s.t. A[i, j] = 1.
-        found = False
-        for idx_first_one in range(j, A_copy.shape[0]):
-            if A_copy[idx_first_one, j]:
-                found = True
-                break
-        # Swap that row with the j^th row.
-        A_copy = _swap_row(A_copy, idx_first_one, j)
-        b_copy = _swap_elems(b_copy, idx_first_one, j)
-        # Eliminate all other rows i where A[i, j] = 1.
-        for i in range(j+1, A.shape[0]):
-            if A_copy[i, j]:
-                A_copy[i, :] = A_copy[i, :] ^ A_copy[j, :]
-                b_copy[i] = b_copy[i] ^ b_copy[j]
-    return A_copy, b_copy
-
-
-def _boolean_backsub_solve(A_rref: np.ndarray, b_rref: np.ndarray) -> np.ndarray:
-    x = np.zeros(A_rref.shape[1]).astype(bool)
-    # Find the first value of i s.t. A[i, i] != 1.
-    found = False
-    for first_i in range(A_rref.shape[1]):
-        if not A_rref[first_i, first_i]:
-            found = True
-            break
-    if not found:
-        first_i += 1
-    for i in range(first_i - 1, -1, -1):
-        x_i = False
-        for j in range(i+1, first_i):
-            x_i ^= A_rref[i, j] and x[j]
-        x_i ^= b_rref[i]
-        x[i] = x_i
-    return x
-
-
-def solve_boolean_system(A, b):
-    A_rref, b_rref = _boolean_rref(A, b)
-    x = _boolean_backsub_solve(A_rref, b_rref)
-    return x
-
+from encoded.binary_linalg import solve_boolean_system
 
 def generators_to_matrix(generators: List[stim.PauliString]) -> np.ndarray:
     rows = []
--- encoded/test_decompose_operators.py
+++ encoded/test_decompose_operators.py
@@ -2,85 +2,9 @@ import unittest
 import numpy as np
 import stim
 from encoded.decompose_operators import (
-    _boolean_rref, _boolean_backsub_solve,
-    solve_boolean_system,
     decompose_operator_to_product,
     decompose_pauli_to_logical_operators
 )
-
-class TestRREF(unittest.TestCase):
-
-    def test_id(self):
-        A = np.eye(3).astype(bool)
-        b = np.array([True, False, False])
-        A_rref, b_rref = _boolean_rref(A, b)
-        self.assertTrue(np.allclose(A_rref, A) and np.allclose(b_rref, b))
-    
-    def test_one_below_diagonal(self):
-        A = np.array([
-            [True, False],
-            [True, True]
-        ])
-        b = np.array([True, True])
-        A_target = np.eye(2).astype(bool)
-        b_target = np.array([True, False])
-        A_rref, b_rref = _boolean_rref(A, b)
-        self.assertTrue(np.allclose(A_target, A_rref) and np.allclose(b_target, b_rref))
-    
-    def test_zzi_izz_ziz(self):
-        A = np.array([
-            [True, False, True],
-            [True, True, False],
-            [False, True, True]
-        ])
-        b = np.zeros(3).astype(bool)
-        A_rref, b_rref = _boolean_rref(A, b)
-        A_target = np.array([
-            [True, False, True],
-            [False, True, True],
-            [False, False, False]
-        ])
-        self.assertTrue(np.allclose(A_rref, A_target))
-
-
-class TestBacksub(unittest.TestCase):
-
-    def test_id(self):
-        A = np.eye(3).astype(bool)
-        b = np.array([True, False, False])
-        x = _boolean_backsub_solve(A, b)
-        self.assertTrue(np.allclose(x, b))
-    
-    def test_one_above_diagonal(self):
-        A = np.array([
-            [True, True],
-            [False, True]
-        ])
-        b = np.array([True, False])
-        x = _boolean_backsub_solve(A, b)
-        x_target = np.array([True, False])
-        self.assertTrue(np.allclose(x, x_target))
-
-
-class TestLinearSolve(unittest.TestCase):
-
-    def test_id(self):
-        A = np.eye(3).astype(bool)
-        b = np.array([True, False, False])
-        x = solve_boolean_system(A, b)
-        self.assertTrue(np.allclose(x, b))
-
-    def test_zzi_izz_ziz(self):
-        A = np.array([
-            [True, False, True],
-            [True, True, False],
-            [False, True, True]
-        ])
-        b = np.array([True, False, True])
-        x_target = np.array([True, True, False])
-        x = solve_boolean_system(A, b)
-        self.assertTrue(np.allclose(x, x_target))
-
 
 class TestDecompose(unittest.TestCase):
 
