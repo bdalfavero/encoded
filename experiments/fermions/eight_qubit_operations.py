010d8f289b9f8b6f30208c958e3876d932a6e34c
Benjamin DalFavero <bdalfavero@gmail.com>
2026-04-27 11:48:04 -0400

    Got logical operators (X, Z) and started on circuit.

--- experiments/fermions/eight_qubit_operations.py
+++ experiments/fermions/eight_qubit_operations.py
@@ -1,24 +1,29 @@
 import stim
+import htlogicalgates as htlg
 from encoded.utils import get_observables
 from encoded.prep_circuit import cb_prep_circuit
-from encoded.utils import stim_pauli_string_to_circuit
-from encoded.logical_circuits import logical_cnot
+
+def stim_pauli_string_to_htlg_str(ps: stim.PauliString) -> str:
+    ps_str = ""
+    for i, p in enumerate(ps):
+        if p == 1:
+            ps_str += f"X{i} "
+        elif p == 2:
+            ps_str += f"Y{i} "
+        elif p == 3:
+            ps_str += f"Z{i} "
+        else:
+            continue
+    return ps_str[:-1]
 
 stabilizers = [
     stim.PauliString("Z_Z_Z_Z_"),
     stim.PauliString("_Z_Z_Z_Z")
 ]
-logical_ops = get_observables(stabilizers)
-# for x, z in logical_ops:
-#     print("x=", x)
-#     print("z=", z)
-logical_zs = [t[1] for t in logical_ops]
-logicals_xs = [t[0] for t in logical_ops]
-logical_x_circuits = [stim_pauli_string_to_circuit(xbar) for xbar in logicals_xs]
+stabs_htlg = [stim_pauli_string_to_htlg_str(stab) for stab in stabilizers]
 
-total_ckt = stim.Circuit()
-zero_prep_ckt = cb_prep_circuit(stabilizers, logical_zs, [False] * len(logical_zs))
-total_ckt += zero_prep_ckt
-for i in range(len(logicals_xs)):
-    if i % 2 == 0:
-        total_ckt += logical_x_circuits[i]
+logical_ops = get_observables(stabilizers)
+logical_zs = [t[1] for t in logical_ops]
+logical_xs = [t[0] for t in logical_ops]
+logical_zs_htlg = [stim_pauli_string_to_htlg_str(zbar) for zbar in logical_zs]
+logical_xs_htlg = [stim_pauli_string_to_htlg_str(xbar) for xbar in logical_xs]
