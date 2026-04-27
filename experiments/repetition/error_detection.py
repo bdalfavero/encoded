77479a89bbea7160f6f02d3fcbc9549652f10fa0
Benjamin DalFavero <bdalfavero@gmail.com>
2026-04-27 11:47:10 -0400

    Got logical circuit for a Hadamard.

---
+++ experiments/repetition/error_detection.py
@@ -0,0 +1,49 @@
+import stim
+import htlogicalgates as htlg
+from encoded.utils import get_observables
+from encoded.prep_circuit import cb_prep_circuit
+
+def stim_pauli_string_to_htlg_str(ps: stim.PauliString) -> str:
+    ps_str = ""
+    for i, p in enumerate(ps):
+        if p == 1:
+            ps_str += f"X{i} "
+        elif p == 2:
+            ps_str += f"Y{i} "
+        elif p == 3:
+            ps_str += f"Z{i} "
+        else:
+            continue
+    return ps_str[:-1]
+
+
+stabilizers = [
+    stim.PauliString("ZZ_"),
+    stim.PauliString("_ZZ")
+]
+logical_ops = get_observables(stabilizers)
+logical_zs = [t[1] for t in logical_ops]
+logical_xs = [t[0] for t in logical_ops]
+print("Logical Z:")
+for zbar in logical_zs:
+    print(zbar)
+print("Logical X:")
+for xbar in logical_xs:
+    print(xbar)
+
+stabs_htlg = [stim_pauli_string_to_htlg_str(stab) for stab in stabilizers]
+logical_zs_htlg = [stim_pauli_string_to_htlg_str(zbar) for zbar in logical_zs]
+logical_xs_htlg = [stim_pauli_string_to_htlg_str(xbar) for xbar in logical_xs]
+
+encoding_ckt = cb_prep_circuit(stabilizers, logical_zs, [False])
+print("Encoding circuit")
+print(encoding_ckt)
+
+# Get the physical version of the logical circuit.
+logical_gate = htlg.Circuit(1)
+logical_gate.h(0)
+stab_code = htlg.StabilizerCode(logical_xs_htlg, logical_zs_htlg, stabs_htlg)
+connectivity = htlg.Connectivity("circular", num_qubits=3)
+logical_circ, status = htlg.tailor_logical_gate(stab_code, connectivity, logical_gate, num_cz_layers=2)
+print("Logical circuit")
+print(logical_circ)
