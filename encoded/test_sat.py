da20b9995decf6a8baa7ef41727f051a0e3f8c92
Benjamin DalFavero <bdalfavero@gmail.com>
2026-06-01 13:21:17 -0400

    Turned the example into a unit test.

---
+++ encoded/test_sat.py
@@ -0,0 +1,19 @@
+import unittest
+import stim
+from encoded.stabilizer_code import StabilizerCode
+from encoded.sat import solve_single_stabilizer
+
+class TestSingleStabilizer(unittest.TestCase):
+
+    def test_zzi_detect_iix(self):
+        stabilizers = [stim.PauliString("ZZI")]
+        code = StabilizerCode.from_stim(stabilizers)
+        err = stim.PauliString("IIX")
+        new_stabilizer = solve_single_stabilizer(code, err, pad=False)
+        comm_tests = [new_stabilizer.commutes(stabilizer) for stabilizer in stabilizers]
+        err_comm_test = not new_stabilizer.commutes(err)
+        self.assertTrue(all(comm_tests + [err_comm_test]))
+
+
+if __name__ == "__main__":
+    unittest.main()
