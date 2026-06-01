3a45bea7a0a6080ddbf15bbde3e55470b63b863a
Benjamin DalFavero <dalfabd2@ffqlgpv3qm-ml.jhuapl.edu>
2026-06-01 10:14:25 -0400

    Fixed a bug (had arguments swapped around in the sympy implementation) and then did the same calculation with pysat.

--- experiments/sat/symplectic_ip_ncf.py
+++ experiments/sat/symplectic_ip_ncf.py
@@ -3,9 +3,18 @@ boolean vectors. For two vectors [x_1 z_1] and [x_2 z_2], their symplectic inner
 x_1 z_1 ^ x_2 z_2, where ^ denotes exclusive OR."""
 
 from sympy import *
+from pysat.formula import Atom, And, XOr
 
 x1, z1, x2, z2 = symbols("x1,z1,x2,z2")
-symplectic_ip = (x1 & z1) ^ (x2 & z2)
+symplectic_ip = (x1 & z2) ^ (x2 & z1)
 print("Original expression:", symplectic_ip)
 symplectic_ip_cnf = to_cnf(symplectic_ip)
 print("CNF:", symplectic_ip_cnf)
+
+# Repeat with pysat.
+x1, z1, x2, z2 = Atom(1), Atom(2), Atom(3), Atom(4)
+symplectic_ip = XOr(And(x1, z2), And(z1, x2))
+print("Pysat formula", symplectic_ip)
+symplectic_ip.clausify()
+print("Clauses from pysat:", symplectic_ip.clauses)
+breakpoint()
